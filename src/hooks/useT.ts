be251ce502435329a1b4622604a7b8b17912b1d4
Felipe Saturnino <felipe.saturnino@spingaming.com.br>
2026-02-25 19:45:32 -0300

    Delete src/hooks directory

--- src/hooks/useT.ts
+++
@@ -1,7 +0,0 @@
-import { useApp } from "../context/AppContext";
-import { translations } from "../constants/i18n";
-
-export function useT() {
-  const { lang } = useApp();
-  return translations[lang];
-}
