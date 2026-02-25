3a4db09d99cf85e9ab977387a181cfff973f44e3
Felipe Saturnino <felipe.saturnino@spingaming.com.br>
2026-02-25 20:29:13 -0300

    Update index.tsx

--- src/pages/geral/Ajuda/index.tsx
+++ src/pages/geral/Ajuda/index.tsx
@@ -1,4 +1,4 @@
-import UnderConstruction from "../../components/UnderConstruction";
+import UnderConstruction from "../../../components/UnderConstruction";
 export default function Ajuda() {
   return <UnderConstruction label="Ajuda" />;
 }
